d7e95b635ed8af0b1bf7fe32fcf49aaf0e064d95
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-10-22 22:09:40 +0900

    Updates

---
+++ precache-manifest.b165b9780f1153c3ad7523afcbafabf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34da313ba886febf2e9508ffea08194d",
+    "url": "/binary_clock/index.html"
+  },
+  {
+    "revision": "7374bbf1cbd9db886926",
+    "url": "/binary_clock/static/css/main.6d3c34af.chunk.css"
+  },
+  {
+    "revision": "f11cbcf3703270e084f8",
+    "url": "/binary_clock/static/js/2.7351968e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/binary_clock/static/js/2.7351968e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7374bbf1cbd9db886926",
+    "url": "/binary_clock/static/js/main.57472936.chunk.js"
+  },
+  {
+    "revision": "a3cc3b9c75b9e3c23475",
+    "url": "/binary_clock/static/js/runtime-main.53d50b6f.js"
+  }
+]);
